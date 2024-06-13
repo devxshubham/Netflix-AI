06f610aba2a121ad9ebed5923182e25c4879c3e4
devxshubham <devshubhamcode@gmail.com>
2024-06-13 18:48:19 +0530

    integrated gemini api

--- src/components/AIsearch.jsx
+++ src/components/AIsearch.jsx
@@ -1,9 +1,30 @@
 import { useSelector } from "react-redux";
 import { NETFLIX_BACKGROUND } from "../utils/constants";
 import languages from "../utils/languageConstant";
+import { useRef } from "react";
+
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 function AIsearch() {
-  const currLanguage = useSelector( store => store.language.currLang);
+  const inputRef = useRef();
+
+  const currLanguage = useSelector((store) => store.language.currLang);
+
+  
+
+  const handleAIsearch = async () => {
+    const genAI = new GoogleGenerativeAI(process.env.REACT_APP_GEMINI_API);
+
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+    const prompt = "Act as a movie recommendation system and suggest some movies based on the queary : "+inputRef.current+". only give me names of 5 movies. return movies separated by commas"
+    
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    console.log(response)
+    const text = response.text();
+    console.log(text);
+  };
 
   return (
     <div className="flex flex-col h-screen bg-black items-center justify-center">
@@ -17,11 +38,15 @@ function AIsearch() {
       </div>
       <div className="flex justify-center rounded-lg z-10 bg-black bg-opacity-80 h-[30%] w-[40%] gap-5 items-center">
         <input
-          className="border-none rounded py-2 px-5 w-72 text-white"
+          onChange={(e) => (inputRef.current = e.target.value)}
+          className="border-none text-black rounded py-2 px-5 w-72 "
           type="text"
           placeholder={languages.AIsearchPlaceholder[currLanguage]}
         />
-        <button className="bg-[#e80c14] py-2 rounded px-5 text-white">
+        <button
+          className="bg-[#e80c14] py-2 rounded px-5 text-white"
+          onClick={handleAIsearch}
+        >
           {languages.AIsearch[currLanguage]}
         </button>
       </div>
