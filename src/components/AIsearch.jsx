e3f5932c4cdf009cdfaa0693c07a809467b88024
devxshubham <devshubhamcode@gmail.com>
2024-06-14 19:11:20 +0530

    AI search page completed

--- src/components/AIsearch.jsx
+++ src/components/AIsearch.jsx
@@ -1,42 +1,91 @@
-import { useSelector } from "react-redux";
-import { NETFLIX_BACKGROUND } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { NETFLIX_BACKGROUND, OPTIONS } from "../utils/constants";
 import languages from "../utils/languageConstant";
 import { useRef } from "react";
 
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import { addAImovies } from "../utils/store/AIsearchSlice";
+import AIrecommendedMovies from "./showAIrecommendedMoveis";
 
 function AIsearch() {
   const inputRef = useRef();
+  const dispatch = useDispatch();
 
   const currLanguage = useSelector((store) => store.language.currLang);
 
-  
+  const TMDBsearchMovie = async (movie) => {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/search/movie?query=${movie}`,
+      OPTIONS
+    );
+    const data = await response.json();
+
+    return data.results;
+  };
+
+  const filterTMDBresults = (TMDBresult, MovieNameArray) => {
+    let x = 0;
+    const filteredResults = [];
+    console.log(TMDBresult);
+    TMDBresult?.map((movies) => {
+      let flag = 0;
+      for (let i = 0; i < movies.length; i++) {
+        if (movies[i].title == MovieNameArray[x]) {
+          filteredResults.push(movies[i]);
+          flag = 1;
+          break;
+        }
+      }
+      if (flag == 0) filteredResults.push(movies[0]);
+      x++;
+    });
+    return filteredResults;
+  };
 
   const handleAIsearch = async () => {
     const genAI = new GoogleGenerativeAI(process.env.REACT_APP_GEMINI_API);
 
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-    const prompt = "Act as a movie recommendation system and suggest some movies based on the queary : "+inputRef.current+". only give me names of 5 movies. return movies separated by commas"
-    
+    const prompt =
+      "Act as a movie recommendation system and suggest some movies based on the queary : " +
+      inputRef.current +
+      ". only give me names of 5 movies. return movies separated by commas always and nothing else in response";
+
     const result = await model.generateContent(prompt);
     const response = await result.response;
-    console.log(response)
-    const text = response.text();
-    console.log(text);
+
+    const MovieNameArray =
+      response?.candidates[0]?.content?.parts[0]?.text?.split(", ");
+    const TMDBresultsPromise = MovieNameArray?.map((movie) =>
+      TMDBsearchMovie(movie)
+    );
+    console.log(
+      "GEMINI RESPONSE",
+      response?.candidates[0]?.content?.parts[0]?.text
+    );
+    const TMDBresult = await Promise.all(TMDBresultsPromise);
+
+    console.log(TMDBresult);
+
+    const AIrecommendedMovies = filterTMDBresults(TMDBresult, MovieNameArray);
+    console.log(AIrecommendedMovies);
+
+    dispatch(addAImovies(AIrecommendedMovies));
   };
 
   return (
-    <div className="flex flex-col h-screen bg-black items-center justify-center">
-      <div className="absolute z-5 w-screen h-screen">
-        <div className="absolute z-5 bg-black bottom-0 w-screen h-screen opacity-60"></div>
+    <div>
+    <div className="flex flex-col h-screen bg-black items-center justify-center overflow-x-scroll">
+      <div className="absolute  md:w-screen h-screen">
+        <div className="absolute bg-black bottom-0 w-screen h-screen opacity-60"></div>
         <img
           className="w-screen h-screen z-0 object-cover	"
           src={NETFLIX_BACKGROUND}
           alt="background"
         />
       </div>
-      <div className="flex justify-center rounded-lg z-10 bg-black bg-opacity-80 h-[30%] w-[40%] gap-5 items-center">
+      <div className="flex flex-col md:flex-row  justify-center items-center px-10 py-20 rounded-lg z-10 bg-black bg-opacity-80  gap-5 ">
         <input
           onChange={(e) => (inputRef.current = e.target.value)}
           className="border-none text-black rounded py-2 px-5 w-72 "
@@ -51,6 +100,11 @@ function AIsearch() {
         </button>
       </div>
       <div className="text-white">recommendations</div>
+      
+    </div>
+    <div className="absolute bottom-10 z-10 flex items-start">
+        <AIrecommendedMovies />
+      </div>
     </div>
   );
 }
--- src/utils/store/AIsearchSlice.js
+++ src/utils/store/AIsearchSlice.js
@@ -4,13 +4,17 @@ const AIsearchSlice = createSlice({
   name: "AIsearchSlice",
   initialState: {
     showSearch: false,
+    AImovies : [],
   },
   reducers: {
     toggleState: (state) => {
       state.showSearch = !state.showSearch;
     },
+    addAImovies : (state,action)=>{
+      state.AImovies = action.payload;
+    }
   },
 });
-export const { toggleState } = AIsearchSlice.actions;
+export const { toggleState, addAImovies } = AIsearchSlice.actions;
 
 export default AIsearchSlice.reducer;
